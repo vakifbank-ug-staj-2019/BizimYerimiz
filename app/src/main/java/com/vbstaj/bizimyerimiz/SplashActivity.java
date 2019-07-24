8ff1fb9e70de4b0b07d1aa675d716494aac3844b
Mert KARAKAYA <mertkarakaya1611@gmail.com>
2019-07-24 13:38:04 +0300

    Splash Activity'nin, Main Activity'e bağlanması sağlandı. Splash tasarımı tamam Main Activity tasarımının temeli bitti diğer tüm activitlerde main activity'nin temeli kullanılacak

--- app/src/main/java/com/vbstaj/bizimyerimiz/SplashActivity.java
+++ app/src/main/java/com/vbstaj/bizimyerimiz/SplashActivity.java
@@ -2,6 +2,7 @@ package com.vbstaj.bizimyerimiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,5 +11,25 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
